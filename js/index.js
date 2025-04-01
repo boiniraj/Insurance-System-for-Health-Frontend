b566d87b39ec4da9b435d8a31feb8de890c8d37f
boiniraj <boinirajkumar02@gmail.com>
2025-04-01 19:48:52 +0530

    intial commit

--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ document.getElementById('activateError').textContent = "Passwords do not match."
 return;
 }
 
-fetch('http://localhost:7079/user-api/activate-User', {
+fetch('http://localhost:7777/user-api/activate-User', {
 method: 'POST',
 headers: { 'Content-Type': 'application/json' },
 body: JSON.stringify({ email, tempPassword, confirmPassword }) // Fix: Use confirmPassword, not newPassword
@@ -91,7 +91,7 @@ function login() {
 let email = document.getElementById('loginEmail').value;
 let password = document.getElementById('loginPassword').value;
 
-fetch('http://localhost:7079/user-api/login-User', {
+fetch('http://localhost:7777/user-api/login-User', {
 method: 'POST',
 headers: { 'Content-Type': 'application/json' },
 body: JSON.stringify({ email, password })
